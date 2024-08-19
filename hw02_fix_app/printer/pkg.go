ccb62d18af20b3b786ff698edefb5789344a9fbd
MoshKillaPit <dimakornet@gmail.com>
2024-08-19 17:23:58 +0300

    HW2 is completed

--- hw02_fix_app/printer/pkg.go
+++ hw02_fix_app/printer/pkg.go
@@ -1,15 +1,13 @@
 package printer
 
 import (
+	"fmt"
+
 	"github.com/fixme_my_friend/hw02_fix_app/types"
 )
 
-func PrintStaff(staff types.Employee) {
-	var str string
-	for i := 0; i < len(staff); i++ {
-		str := fmt.Sprintf("User ID: %d; Age: %d; Name: %s; Department ID: %d; ", staff[i].UserID, staff[i].Age, staff[i].Name, staff[i].DepartmentID)
-		fmt.Println(str)
+func PrintStaff(staff []types.Employee) {
+	for _, v := range staff {
+		fmt.Println(v.String())
 	}
-
-	fmt.Println(str)
 }
