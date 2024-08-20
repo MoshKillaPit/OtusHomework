96a059211aa2c54a315644f8ca1819a9020366c7
MoshKillaPit <dimakornet@gmail.com>
2024-08-20 15:49:16 +0300

    hw02_fix_app_fixed02

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/fixme_my_friend/hw02_fix_app/printer"
 	"github.com/fixme_my_friend/hw02_fix_app/reader"
 	"github.com/fixme_my_friend/hw02_fix_app/types"
 )
 
 func main() {
-	var path string = "data.json"
-
+	path := "data.json"
 	fmt.Printf("Enter data file path: ")
 	fmt.Scanln(&path)
 
@@ -18,11 +18,9 @@ func main() {
 
 	if len(path) == 0 {
 		path = "data.json"
-	} else {
 	}
 
 	staff, err = reader.ReadJSON(path)
-
 	if err != nil {
 		panic(err)
 	}
--- hw02_fix_app/reader/pkg.go
+++ hw02_fix_app/reader/pkg.go
@@ -12,19 +12,19 @@ import (
 func ReadJSON(filePath string) ([]types.Employee, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
-		return nil, fmt.Errorf("could not open file: %v", err)
+		return nil, fmt.Errorf("could not open file: %w", err)
 	}
 	defer f.Close()
 
 	byteJSON, err := io.ReadAll(f)
 	if err != nil {
-		return nil, fmt.Errorf("could not read file: %v", err)
+		return nil, fmt.Errorf("could not read file: %w", err)
 	}
 
 	var data []types.Employee
 	err = json.Unmarshal(byteJSON, &data)
 	if err != nil {
-		return nil, fmt.Errorf("could not unmarshal JSON: %v", err)
+		return nil, fmt.Errorf("could not unmarshal JSON: %w", err)
 	}
 
 	return data, nil
