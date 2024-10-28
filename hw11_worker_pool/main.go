c99a2716ceab9b4828a0b9787c5af3ef68d3ebeb
MoshKillaPit <dimakornet@gmail.com>
2024-10-28 18:57:07 +0300

    HW11Done

--- hw11_worker_pool/main.go
+++ hw11_worker_pool/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+func worker(countGorutines int) int {
+	wg := sync.WaitGroup{}
+	mu := sync.Mutex{}
+	number := 2
+	wg.Add(countGorutines)
+	for i := 0; i < countGorutines; i++ {
+		go func(id int) {
+			defer wg.Done()
+			mu.Lock()
+			number += 1
+			fmt.Printf("Выполнилась %d горутина, результат %d\n", id, number)
+			defer mu.Unlock()
+		}(i)
+	}
+	wg.Wait()
+	fmt.Println("Итоговое значение", number)
+	return number
+}
+
 func main() {
-	// Place your code here.
+
+	countGorutines := 100
+
+	worker(countGorutines)
+
 }
